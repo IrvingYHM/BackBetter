61e6ba6ae2d66f1e953c98b6ce7b06d470d0bfc0
IrvingYHM <hernandezirving725@gmail.com>
2025-07-23 00:54:40 -0600

    detalle de pedidos

--- src/routes/DetallesPedidos.routes.js
+++ src/routes/DetallesPedidos.routes.js
@@ -3,8 +3,8 @@ const router = express.Router();
 const DetallePedido = require("../controllers/Pedido/DetallePedido.controller");
 
 // Ruta para obtener todos los clientes
-router.get("/TodosDetalles", DetallePedido.getAllDetallePedidos);
-router.post("/crear", DetallePedido.CrearDetallePedido);
+router.get("/TodosDetalles", DetallePedido.VerDetallePedido);
+router.post("/crear", DetallePedido.createDetallePedido);
 
 
 
